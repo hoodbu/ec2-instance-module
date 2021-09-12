54b7e2017769959a3a30d10ec4a0e8d91acce2bf
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-09-12 10:56:09 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,78 @@
+##################################################################
+# Data source to get AMI details
+##################################################################
+data "aws_ami" "ubuntu" {
+  provider    = aws.west
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+locals {
+  ubu_user_data = <<EOF
+#!/bin/bash
+sudo hostnamectl set-hostname "Ubuntu"
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo echo 'ubuntu:${var.ace_password}' | /usr/sbin/chpasswd
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get -y install traceroute unzip build-essential git gcc hping3 apache2 net-tools
+sudo apt autoremove
+sudo /etc/init.d/ssh restart
+sudo echo "<html><h1>Aviatrix is awesome</h1></html>" > /var/www/html/index.html 
+EOF
+}
+
+module "my-vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.222.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.222.1.0/24", "10.222.2.0/24", "10.222.3.0/24"]
+  public_subnets  = ["10.222.101.0/24", "10.222.102.0/24", "10.222.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Name = "2021-09"
+  }
+}
+
+module "security_group_1" {
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "~> 3.0"
+  name                = "security_group_spoke1"
+  description         = "Security group for example usage with EC2 instance"
+  vpc_id              = module.my-vpc.vpc_id
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "ssh-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+  providers = {
+    aws = aws.west
+  }
+}
+
+module "aws_ubu_1" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  instance_type               = "t3.micro"
+  name                        = "AWS-ubu"
+  ami                         = data.aws_ami.ubuntu.id
+  key_name                    = var.EW1_ec2_key_name
+  subnet_id                   = module.my-vpc.public_subnets[0].subnet_id
+  vpc_security_group_ids      = [module.security_group_1.this_security_group_id]
+  associate_public_ip_address = true
+  user_data_base64            = base64encode(local.ubu_user_data)
+  providers = {
+    aws = aws.west
+  }
+}
