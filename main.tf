34c68bee4b9e7be36e4fd7846f929cb2e70e8d05
hoodbu <38543715+hoodbu@users.noreply.github.com>
2021-09-12 15:00:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,9 +97,9 @@ output "Public_IP" {
 
 ### Code below needs to output Private IP.
 
-/* 
 data "aws_network_interface" "aws_ubu_1" {
-  id = output.primary_network_interface_id
+  provider = aws.west
+  id       = moduleaws_ubu_1.primary_network_interface_id
 }
   
 # private_ip = data.aws_network_interface.aws_ubu_1.private_ip
@@ -107,5 +107,3 @@ data "aws_network_interface" "aws_ubu_1" {
 output "Private_IP" {
   value = data.aws_network_interface.aws_ubu_1.private_ip
 }
- 
-*/
